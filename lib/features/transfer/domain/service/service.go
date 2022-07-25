5aae11dbd3b43fa1ef3030fa842346d990031d36
Sam Komarov <sam@skomarov.com>
2022-07-25 12:17:10 +0300

    finish implementing Transferer

--- lib/features/transfer/domain/service/service.go
+++ lib/features/transfer/domain/service/service.go
@@ -3,8 +3,12 @@ package service
 import (
 	"fmt"
 	"github.com/k0marov/avencia-api-contract/api/client_errors"
+	"github.com/k0marov/avencia-backend/lib/core"
+	"github.com/k0marov/avencia-backend/lib/core/batch"
 	"github.com/k0marov/avencia-backend/lib/core/core_errors"
+	"github.com/k0marov/avencia-backend/lib/core/firestore_facade"
 	atmService "github.com/k0marov/avencia-backend/lib/features/atm_transaction/domain/service"
+	transValues "github.com/k0marov/avencia-backend/lib/features/atm_transaction/domain/values"
 	"github.com/k0marov/avencia-backend/lib/features/auth"
 	"github.com/k0marov/avencia-backend/lib/features/transfer/domain/values"
 )
@@ -14,16 +18,44 @@ type Transferer = func(values.RawTransfer) error
 // transferConverter error may be a ClientError
 type transferConverter = func(values.RawTransfer) (values.Transfer, error)
 
-func NewTransferer(convert transferConverter, transact atmService.TransactionFinalizer) Transferer {
+// TODO: try to simplify
+func NewTransferer(convert transferConverter, runBatch batch.WriteRunner, transact atmService.TransactionFinalizer) Transferer {
 	return func(raw values.RawTransfer) error {
 		if raw.Money.Amount < 0 {
 			return client_errors.NegativeTransferAmount
 		}
-		_, err := convert(raw)
+		t, err := convert(raw)
 		if err != nil {
-			return fmt.Errorf("while converting raw transfer data to a transfer: %w", err)
+			return fmt.Errorf("converting raw transfer data to a transfer: %w", err)
 		}
-		return nil
+
+		return runBatch(func(u firestore_facade.BatchUpdater) error {
+			// withdraw money from the wallet of caller
+			withdrawTrans := transValues.Transaction{
+				UserId: t.FromId,
+				Money: core.Money{
+					Currency: t.Money.Currency,
+					Amount:   -t.Money.Amount,
+				},
+			}
+			err = transact(u, withdrawTrans)
+			if err != nil {
+				return fmt.Errorf("withdrawing money from caller: %w", err)
+			}
+			// deposit money to recipient
+			depositTrans := transValues.Transaction{
+				UserId: t.ToId,
+				Money: core.Money{
+					Currency: t.Money.Currency,
+					Amount:   t.Money.Amount,
+				},
+			}
+			err = transact(u, depositTrans)
+			if err != nil {
+				return fmt.Errorf("depositing money to recipeint: %w", err)
+			}
+			return nil
+		})
 	}
 }
 
--- lib/features/transfer/domain/service/service_test.go
+++ lib/features/transfer/domain/service/service_test.go
@@ -1,29 +1,34 @@
 package service_test
 
 import (
+	"cloud.google.com/go/firestore"
 	"github.com/k0marov/avencia-api-contract/api/client_errors"
+	"github.com/k0marov/avencia-backend/lib/core"
 	"github.com/k0marov/avencia-backend/lib/core/core_errors"
+	"github.com/k0marov/avencia-backend/lib/core/firestore_facade"
 	. "github.com/k0marov/avencia-backend/lib/core/test_helpers"
+	transValues "github.com/k0marov/avencia-backend/lib/features/atm_transaction/domain/values"
 	"github.com/k0marov/avencia-backend/lib/features/auth"
 	"github.com/k0marov/avencia-backend/lib/features/transfer/domain/service"
 	"github.com/k0marov/avencia-backend/lib/features/transfer/domain/values"
 	"testing"
 )
 
-// TODO: add checking if Money.Amount is negative in Transferer, otherwise everyone will be able to transfer money to their account from other users :)
-
 func TestTransferer(t *testing.T) {
 	tRaw := RandomRawTransfer()
-	//transf := RandomTransfer()
+	transf := RandomTransfer()
 	t.Run("error case - money.amount is negative", func(t *testing.T) {
 		tRaw := values.RawTransfer{
 			FromId:  RandomString(),
 			ToEmail: RandomString(),
 			Money:   RandomNegativeMoney(),
 		}
-		err := service.NewTransferer(nil, nil)(tRaw)
+		err := service.NewTransferer(nil, nil, nil)(tRaw)
 		AssertError(t, err, client_errors.NegativeTransferAmount)
 	})
+	convert := func(values.RawTransfer) (values.Transfer, error) {
+		return transf, nil
+	}
 	t.Run("error case - converting transfer throws", func(t *testing.T) {
 		convert := func(gotTransf values.RawTransfer) (values.Transfer, error) {
 			if gotTransf == tRaw {
@@ -31,17 +36,57 @@ func TestTransferer(t *testing.T) {
 			}
 			panic("unexpected")
 		}
-		err := service.NewTransferer(convert, nil)(tRaw)
+		err := service.NewTransferer(convert, nil, nil)(tRaw)
 		AssertSomeError(t, err)
 	})
-	t.Run("making withdraw from caller transaction fails", func(t *testing.T) {
 
+	// TODO: simplify this callback hell
+	runBatch := func(f func(firestore_facade.BatchUpdater) error) error {
+		return f(func(*firestore.DocumentRef, map[string]any) error {
+			return nil
+		})
+	}
+
+	withdrawTrans := transValues.Transaction{
+		UserId: transf.FromId,
+		Money: core.Money{
+			Currency: transf.Money.Currency,
+			Amount:   -transf.Money.Amount,
+		},
+	}
+	depositTrans := transValues.Transaction{
+		UserId: transf.ToId,
+		Money: core.Money{
+			Currency: transf.Money.Currency,
+			Amount:   transf.Money.Amount,
+		},
+	}
+
+	transact := func(firestore_facade.BatchUpdater, transValues.Transaction) error { return nil }
+
+	t.Run("error case - withdrawing from caller fails", func(t *testing.T) {
+		transact := func(u firestore_facade.BatchUpdater, t transValues.Transaction) error {
+			if t == withdrawTrans {
+				return RandomError()
+			}
+			return nil
+		}
+		err := service.NewTransferer(convert, runBatch, transact)(tRaw)
+		AssertSomeError(t, err)
 	})
-	t.Run("making deposit to recepient transaction fails", func(t *testing.T) {
-
+	t.Run("error case - depositing to recipient fails", func(t *testing.T) {
+		transact := func(u firestore_facade.BatchUpdater, t transValues.Transaction) error {
+			if t == depositTrans {
+				return RandomError()
+			}
+			return nil
+		}
+		err := service.NewTransferer(convert, runBatch, transact)(tRaw)
+		AssertSomeError(t, err)
 	})
 	t.Run("happy case", func(t *testing.T) {
-
+		err := service.NewTransferer(convert, runBatch, transact)(tRaw)
+		AssertNoError(t, err)
 	})
 }
 
