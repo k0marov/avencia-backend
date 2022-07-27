0f0ee1cd89779b497ccf6f388ec1aaec7bf31406
Sam Komarov <sam@skomarov.com>
2022-07-27 12:23:19 +0300

    remove the completed TODO

--- lib/features/transfer/delivery/http/handlers/handlers.go
+++ lib/features/transfer/delivery/http/handlers/handlers.go
@@ -7,8 +7,6 @@ import (
 	"net/http"
 )
 
-// TODO: maybe generalize the http handlers' structure, because they all look the same
-
 func NewTransferHandler(transfer service.Transferer) http.HandlerFunc {
 	return http_helpers.NewAuthenticatedHandler(
 		apiRequests.TransferDecoder,
