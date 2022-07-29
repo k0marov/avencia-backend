fea43c9f87ef20ccb5f9a888376b1e0762661af2
Sam Komarov <sam@skomarov.com>
2022-07-29 11:23:51 +0300

    stop Rethrow from modifying the passed in ClientError

--- lib/core/core_err/core_errors.go
+++ lib/core/core_err/core_errors.go
@@ -3,11 +3,16 @@ package core_err
 import (
 	"errors"
 	"fmt"
+	"github.com/k0marov/avencia-api-contract/api/client_errors"
 )
 
 // ErrNotFound is an error that is passed from store layer to domain layer so that a 404 Client Error is thrown
 var ErrNotFound = errors.New("not found")
 
 func Rethrow(description string, err error) error {
+	clientErr, ok := err.(client_errors.ClientError)
+	if ok {
+		return clientErr
+	}
 	return fmt.Errorf("%s: %w", description, err)
 }
