3e70eb28fcb326a7f96f7c80204210748bf37d5e
Sam Komarov <sam@skomarov.com>
2022-07-30 20:20:20 +0300

    update the store layer to use the new mappers

--- lib/features/limits/store/store.go
+++ lib/features/limits/store/store.go
@@ -7,55 +7,27 @@ import (
 	"cloud.google.com/go/firestore"
 	"github.com/k0marov/avencia-backend/lib/core"
 	"github.com/k0marov/avencia-backend/lib/core/fs_facade"
-	"github.com/k0marov/avencia-backend/lib/core/helpers/general_helpers"
 	"github.com/k0marov/avencia-backend/lib/features/limits/domain/store"
 	"github.com/k0marov/avencia-backend/lib/features/limits/domain/values"
+	"github.com/k0marov/avencia-backend/lib/features/limits/store/mappers"
 )
 
-// withdrawsDocGetter userId should be non-empty
-type withdrawDocGetter = func(userId string, currency core.Currency) *firestore.DocumentRef
-
-func NewWithdrawDocGetter(getDoc fs_facade.DocGetter) withdrawDocGetter {
-	return func(userId string, currency core.Currency) *firestore.DocumentRef {
-		doc := getDoc(fmt.Sprintf("Withdraws/%s/Withdraws/%s", userId, string(currency)))
-		if doc == nil {
-			panic("getting document ref for users's withdraws returned nil. Probably userId is empty.")
-		}
-		return doc
-	}
-}
-
-const withdrawnKey = "withdrawn"
-
-func NewWithdrawsGetter(client *firestore.Client) store.WithdrawsGetter {
+func NewWithdrawsGetter(client *firestore.Client, decode mappers.WithdrawsDecoder) store.WithdrawsGetter {
 	return func(userId string) ([]values.WithdrawnModel, error) {
-		col := client.Collection(fmt.Sprintf("Withdraws/%s/Withdraws", userId))
+		col := client.Collection(fmt.Sprintf("Users/%s/Withdraws", userId))
 		docs, err := col.Documents(context.Background()).GetAll()
 		if err != nil {
-			return map[string]values.WithdrawnModel{}, fmt.Errorf("fetching a list of withdraws documents %w", err)
+			return []values.WithdrawnModel{}, fmt.Errorf("fetching a list of withdraws documents %w", err)
 		}
-
-		withdraws := map[string]values.WithdrawnModel{}
-
-		for _, doc := range docs {
-			withdrawnVal := doc.Data()[withdrawnKey]
-			withdrawn, err := general_helpers.DecodeFloat(withdrawnVal)
-			if err != nil {
-				return map[string]values.WithdrawnModel{}, err
-			}
-			withdraws[doc.Ref.ID] = values.WithdrawnModel{
-				Withdrawn: core.NewMoneyAmount(withdrawn),
-				UpdatedAt: doc.UpdateTime,
-			}
-		}
-		return withdraws, nil
+		
+		return decode(fs_facade.NewDocuments(docs)) 
 	}
 
 }
 
-func NewWithdrawUpdater(getDoc withdrawDocGetter) store.WithdrawUpdater {
+func NewWithdrawUpdater(getDoc fs_facade.DocGetter, encode mappers.WithdrawEncoder) store.WithdrawUpdater {
 	return func(update fs_facade.Updater, userId string, withdrawn core.Money) error {
-		doc := getDoc(userId, withdrawn.Currency)
-		return update(doc, map[string]any{withdrawnKey: withdrawn.Amount.Num()})
+		doc := getDoc(fmt.Sprintf("Users/%s/Withdraws/%s", userId, string(withdrawn.Currency)))
+		return update(doc, encode(withdrawn.Amount))
 	}
 }
