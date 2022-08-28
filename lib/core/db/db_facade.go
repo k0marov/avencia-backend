7290e6e5e0ebdaff87c500ee69bdad9ac15ad187
Sam Komarov <sam@skomarov.com>
2022-08-28 16:22:39 +0300

    finish the json db facade

--- lib/core/db/db_facade.go
+++ lib/core/db/db_facade.go
@@ -1,9 +1,9 @@
 package db
 
-
 type DB struct {
 	db dbInternal
 }
+
 func NewDB(db dbInternal) DB {
 	return DB{
 		db: db,
@@ -11,39 +11,33 @@ func NewDB(db dbInternal) DB {
 
 }
 
-
 type TransactionRunner = func(func(DB) error) error
 
-
-
 type dbInternal interface {
-  Get(path []string) (Document, error)
-  GetCollection(path []string) (Documents, error)
-  Set(path []string, data []byte) error
-  RunTransaction(func(DB) error) error
+	Get(path []string) (Document, error)
+	GetCollection(path []string) (Documents, error)
+	Set(path []string, data []byte) error
+	RunTransaction(func(DB) error) error
 }
 
 type Document struct {
-	Path        []string
-	Data      []byte // TODO: maybe store this as a pointer
+	Path []string
+	Data []byte // TODO: maybe store this as a pointer
 }
-
 
 type Documents []Document
 
 type Getter = func(db DB, path []string) (Document, error)
-type CollectionGetter = func(db DB, colPath []string) (Documents, error) 
+type CollectionGetter = func(db DB, colPath []string) (Documents, error)
 type Setter = func(db DB, path []string, data map[string]any) error
 
-
 func GetterImpl(db DB, path []string) (Document, error) {
-  return db.db.Get(path) 
+	return db.db.Get(path)
 }
 func CollectionGetterImpl(db DB, colPath []string) (Documents, error) {
 	return db.db.GetCollection(colPath)
 }
 
 func SetterImpl(db DB, path []string, data []byte) error {
-  return db.db.Set(path, data)
+	return db.db.Set(path, data)
 }
-
--- lib/core/db/json_db_facade.go
+++ lib/core/db/json_db_facade.go
@@ -11,24 +11,47 @@ type JsonDocument struct {
 	Data map[string]any
 }
 
+type JsonDocuments []JsonDocument
+
 type JsonGetter = func(db DB, path []string) (JsonDocument, error)
-type JsonSetter = func(db DB, path []string, values map[string]any) error
+type JsonCollectionGetter = func(db DB, path []string) (JsonDocuments, error)
+type JsonSetter = func(db DB, path []string, value map[string]any) error
+type JsonUpdater = func(db DB, path []string, value map[string]any) error
+
+func unmarshalDoc(d Document) (JsonDocument, error) {
+	jsonD := JsonDocument{
+		Path: d.Path,
+	}
+	err := json.Unmarshal(d.Data, &jsonD.Data)
+	if err != nil {
+		return JsonDocument{}, core_err.Rethrow("unmarshalling json", err)
+	}
+	return jsonD, nil
+}
 
 func JsonGetterImpl(db DB, path []string) (JsonDocument, error) {
 	doc, err := db.db.Get(path)
 	if err != nil {
 		return JsonDocument{}, core_err.Rethrow("getting raw doc", err)
 	}
-	var jsonDoc map[string]any
-	err = json.Unmarshal(doc.Data, &jsonDoc)
-	if err != nil {
-	  return JsonDocument{}, core_err.Rethrow("unmarshalling json", err)
-	}
+	return unmarshalDoc(doc)
 
-	return JsonDocument{
-		Path: path,
-		Data: jsonDoc,
-	}, nil
+}
+
+func JsonCollectionGetterImpl(db DB, path []string) (JsonDocuments, error) {
+	docs, err := db.db.GetCollection(path)
+	if err != nil {
+		return JsonDocuments{}, core_err.Rethrow("getting raw docs", err)
+	}
+	jDocs := JsonDocuments{} 
+	for _, d := range docs {
+		jDoc, err := unmarshalDoc(d)
+		if err != nil {
+			return JsonDocuments{}, core_err.Rethrow("parsing raw doc", err)
+		}
+		jDocs = append(jDocs, jDoc)
+	}
+	return jDocs, nil
 }
 
 func JsonSetterImpl(db DB, path []string, value map[string]any) error {
@@ -41,14 +64,14 @@ func JsonSetterImpl(db DB, path []string, value map[string]any) error {
 
 func JsonUpdaterImpl(db DB, path []string, value map[string]any) error {
 	return db.db.RunTransaction(func(tDB DB) error {
-    doc, err := JsonGetterImpl(tDB, path)
-    if err != nil {
-    	return core_err.Rethrow("getting current doc", err)
-    }
-    data := doc.Data
-    for k, v := range value {
-    	data[k] = v
-    }
-    return JsonSetterImpl(tDB, path, data)
+		doc, err := JsonGetterImpl(tDB, path)
+		if err != nil {
+			return core_err.Rethrow("getting current doc", err)
+		}
+		data := doc.Data
+		for k, v := range value {
+			data[k] = v
+		}
+		return JsonSetterImpl(tDB, path, data)
 	})
 }
