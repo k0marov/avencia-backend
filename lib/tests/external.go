35ae5ea4750ba2cc95de6abe81f348ada67725b2
Sam Komarov <sam@skomarov.com>
2022-09-06 19:52:23 +0300

    start implementing the integration test

---
+++ lib/tests/external.go
@@ -0,0 +1,55 @@
+package integration_test
+
+import (
+	"testing"
+
+	"github.com/apple/foundationdb/bindings/go/src/fdb"
+	"github.com/k0marov/avencia-backend/lib/core/core_err"
+	"github.com/k0marov/avencia-backend/lib/core/db/foundationdb"
+	. "github.com/k0marov/avencia-backend/lib/core/helpers/test_helpers"
+	"github.com/k0marov/avencia-backend/lib/di"
+	authEntities "github.com/k0marov/avencia-backend/lib/features/auth/domain/entities"
+)
+
+type MockUser struct {
+	Token, Id, Email string
+}
+
+type MockAuth struct {
+	Users []MockUser
+}
+
+func (a MockAuth) Verify(token string) (userId string) {
+	for _, u := range a.Users {
+		if u.Token == token {
+			return u.Id
+		}
+	}
+	return ""
+}
+
+func (a MockAuth) UserByEmail(email string) (authEntities.User, error) {
+	for _, u := range a.Users {
+		if u.Email == email {
+			return authEntities.User{Id: u.Id}, nil
+		}
+	}
+	return authEntities.User{}, core_err.ErrNotFound
+}
+
+func prepareExternalDeps(t *testing.T, users []MockUser) (d di.ExternalDeps, cancelTrans func()) {
+	db := fdb.MustOpenDefault()
+	trans, err := db.CreateTransaction()
+	AssertNoError(t, err)
+	trans.SetReadVersion(1)
+
+	runner := foundationdb.NewTransactionRunner(trans)
+	
+
+	return di.ExternalDeps{
+		AtmSecret: []byte("atm_test"),
+		JwtSecret: []byte("jwt_test"),
+		Auth:      MockAuth{Users: users},
+		TRunner:   runner,
+	}, trans.Cancel
+}
--- lib/tests/integration_test.go
+++ lib/tests/integration_test.go
@@ -1,82 +1,151 @@
-package tests
+package integration_test
 
 import (
+	"bytes"
+	"encoding/json"
+	"math"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
-	"github.com/apple/foundationdb/bindings/go/src/fdb"
-	"github.com/k0marov/avencia-backend/lib/core/core_err"
-	"github.com/k0marov/avencia-backend/lib/core/db/foundationdb"
+	api "github.com/k0marov/avencia-api-contract/api"
+	"github.com/k0marov/avencia-backend/lib/core"
 	. "github.com/k0marov/avencia-backend/lib/core/helpers/test_helpers"
 	"github.com/k0marov/avencia-backend/lib/di"
-	authEntities "github.com/k0marov/avencia-backend/lib/features/auth/domain/entities"
+	"github.com/k0marov/avencia-backend/lib/features/transactions/domain/values"
 )
 
-type MockUser struct {
-	Token, Id, Email string 
-}
+// TODO: maybe get rid of separation between atm auth and user auth and just use atm secret as a special auth token
+// TODO: maybe stop testing the handlers layer in here, but just call services directly 
 
-type MockAuth struct {
-  Users []MockUser 
-}
-
-func (a MockAuth) Verify(token string) (userId string) {
-	for _, u := range a.Users {
-		if u.Token == token {
-			return u.Id
-		}
-	}
-	return "" 
-} 
-
-func (a MockAuth) UserByEmail(email string) (authEntities.User, error) {
-  for _, u := range a.Users {
-  	if u.Email == email {
-  		return authEntities.User{Id: u.Id}, nil
-  	}
-  }
-  return authEntities.User{}, core_err.ErrNotFound
-}
-
-func prepareExternalDeps(t *testing.T, users []MockUser) di.ExternalDeps {
-	db := fdb.MustOpenDefault()
-	trans, err := db.CreateTransaction()
-	AssertNoError(t, err)
-	trans.SetReadVersion(1) 
-  
-  runner := foundationdb.NewTransactionRunner(trans)
-
-  return di.ExternalDeps{
-  	AtmSecret: []byte("atm_test"),
-  	JwtSecret: []byte("jwt_test"),
-  	Auth:      MockAuth{Users: users},
-  	TRunner:   runner,
-  }
-} 
-
-
-// TODO: maybe move middleware usage from the api repository here
+const (
+	AtmAuthSecret = "atm_test"
+	JwtSecret     = "jwt_test"
+)
 
 func TestIntegration(t *testing.T) {
 	users := []MockUser{
 		{
 			Token: RandomString(),
-			Id:    "sam",
+			Id: "sam",
 			Email: "sam@skomarov.com",
-		}, 
+		},
 		{
-			Token: RandomString(),
-			Id:    "john",
-			Email: "test@example.com",
-		}, 
+			RandomString(),
+			"john",
+			"test@example.com",
+		},
 		{
-			Token:RandomString(),
-			Id:    "bill",
-			Email: "test2@example.com",
-		}, 
+			RandomString(),
+			"bill",
+			"test2@example.com",
+		},
 	}
-	apiDeps := di.InitializeBusiness(prepareExternalDeps(t, users))
+	extDeps, cancelDBTrans := prepareExternalDeps(t, users)
+	defer cancelDBTrans()
+	apiDeps := di.InitializeBusiness(extDeps)
 
-	
+	encodeRequest := func(req any) *http.Request {
+		body, err := json.Marshal(req)
+		AssertNoError(t, err)
+		return httptest.NewRequest("", "/asdf", bytes.NewReader(body))
+	}
+	addAuth := func(req *http.Request, authHeader string) *http.Request {
+		req.Header.Add("Authorication", authHeader)
+		return req
+	}
 
+	assertBalance := func(user MockUser, balance core.Money) {
+		request := addAuth(encodeRequest(nil), user.Token)
+		response := httptest.NewRecorder()
+		handler := apiDeps.AuthMW(apiDeps.Handlers.App.GetUserInfo)
+		handler.ServeHTTP(response, request)
+		AssertStatusCode(t, response, http.StatusOK) 
+		var userInfo api.UserInfoResponse
+		err := json.Unmarshal(response.Body.Bytes(), &userInfo)
+		AssertNoError(t, err)
+		Assert(t, userInfo.Wallet[string(balance.Currency)], balance.Amount.Num(), "balance")
+	}
+	generateQRCode := func(user MockUser, transType values.TransactionType) api.GenTransCodeResponse {
+		reqBody := api.GenTransCodeRequest{
+			TransactionType: string(transType),
+		}
+		request := addAuth(encodeRequest(reqBody), user.Token)
+		response := httptest.NewRecorder() 
+		handler := apiDeps.AuthMW(apiDeps.Handlers.App.GenCode)
+		handler.ServeHTTP(response, request) 
+		AssertStatusCode(t, response, http.StatusOK) 
+		var codeResp api.GenTransCodeResponse 
+		err := json.Unmarshal(response.Body.Bytes(), &codeResp)
+		AssertNoError(t, err)
+		return codeResp
+	}
+	startTrans := func(user MockUser, tType values.TransactionType, qrText string) (tId string) {
+		reqBody := api.OnTransactionCreateRequest{
+			TransactionReference: "asdf",
+			TerminalId:           "1234",
+			TerminalSid:          "4321",
+			Type:                 string(tType),
+			QRCodeText:           qrText,
+		}
+		response := httptest.NewRecorder()
+		request := addAuth(encodeRequest(reqBody), AtmAuthSecret)
+		handler := apiDeps.AtmAuthMW(apiDeps.Handlers.Transaction.OnCreate)
+		handler.ServeHTTP(response, request) 
+		AssertStatusCode(t, response, http.StatusOK) 
+		var jsonResp api.OnTransactionCreateResponse 
+		err := json.Unmarshal(response.Body.Bytes(), &jsonResp) 
+		AssertNoError(t, err)
+		return jsonResp.TransactionId
+	}
+	insertBanknote := func(tId string, banknote core.Money) {
+		reqBody := api.BanknoteInsertionRequest{
+			TransactionId: tId,
+			Banknote:      api.Banknote{
+				Currency:     string(banknote.Currency),
+				Denomination: int(math.Round((banknote.Amount.Num()))),
+			},
+			Receivables:   []api.Money{}, // TODO: for now empty, maybe later fix to include receivables
+		}
+		request := addAuth(encodeRequest(reqBody), AtmAuthSecret)
+		response := httptest.NewRecorder() 
+		handler := apiDeps.AtmAuthMW(apiDeps.Handlers.Transaction.Deposit.OnBanknoteEscrow) 
+		handler.ServeHTTP(response, request) 
+		AssertStatusCode(t, response, http.StatusOK) 
+	}
+	deposit := func(user MockUser, tId string, dep core.Money) {
+		reqBody := api.CompleteDepositRequest{
+			TransactionId: tId,
+			Receivables:   []api.Money{{
+				string(dep.Currency),
+				dep.Amount.Num(),
+			}},
+		}
+		request := addAuth(encodeRequest(reqBody), AtmAuthSecret)
+		response := httptest.NewRecorder()
+		handler := apiDeps.AtmAuthMW(apiDeps.Handlers.Transaction.Deposit.OnComplete)
+		handler.ServeHTTP(response, request) 
+    AssertStatusCode(t, response, http.StatusOK)
+	}
+	newMoney := func(curr string, amount float64) core.Money {
+		return core.Money{Currency: core.Currency(curr), Amount: core.NewMoneyAmount(amount)}
+	}
+
+	// assert that balance of user 1 is 0$
+	assertBalance(users[0], newMoney("USD", 0))
+	// assert that balance of user 2 is 0$
+	assertBalance(users[1], newMoney("USD", 0))
+	// deposit 100$ to user 1
+
+	// assert that balance of user 1 is 100$
+	// withdraw 49.5$ from user1
+	// assert that balance of user1 is 50.5$
+	// transfer 10.5$ from user1 to user2
+	// assert that balance of user1 is 40$
+	// assert that balance of user2 is 10.5$
+	// withdraw 4.2$ from user2
+	// assert that balance of user2 is 6.3$
+	// deposit 5000 RUB to user2
+	// assert that balance of user2 is 5000 RUB
 
 }
