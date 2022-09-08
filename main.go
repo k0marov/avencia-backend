1a5a14c84f24237c19d3e9a0bb6bb37f0e3d58b4
root <root@skomarov.com>
2022-09-08 15:13:45 +0300

    add logging the error to the main function

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"net/http"
+	"log"
 
 	"github.com/k0marov/avencia-backend/lib/di"
 	"github.com/k0marov/avencia-backend/lib/di/external"
 )
 
 func main() {
-	http.ListenAndServe(":4244", di.InitializeHandler(di.InitializeBusiness(external.InitializeExternal())))
+	log.Fatalf("while running handler: %v", http.ListenAndServe(":4244", di.InitializeHandler(di.InitializeBusiness(external.InitializeExternal()))))
 }
