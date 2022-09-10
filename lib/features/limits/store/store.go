41af7bab03edc0caab6ab0af5b1b690402e00b67
Sam Komarov <sam@skomarov.com>
2022-09-10 09:05:31 +0300

    fix WithdrawsGetter to handle NotFound correctly

--- lib/features/limits/store/store.go
+++ lib/features/limits/store/store.go
@@ -1,10 +1,10 @@
 package store
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/k0marov/avencia-backend/lib/core"
+	"github.com/k0marov/avencia-backend/lib/core/core_err"
 	"github.com/k0marov/avencia-backend/lib/core/db"
 	"github.com/k0marov/avencia-backend/lib/features/limits/domain/models"
 	"github.com/k0marov/avencia-backend/lib/features/limits/domain/store"
@@ -15,16 +15,15 @@ func NewWithdrawsGetter(getDoc db.JsonGetter, decode mappers.WithdrawsDecoder) s
 	return func(db db.DB, userId string) (models.Withdraws, error) {
 		path := []string{"withdrawn", userId}
 		doc, err := getDoc(db, path)
-		if err != nil {
-			return models.Withdraws{}, fmt.Errorf("fetching a list of withdraws documents %w", err)
+		if err != nil && !core_err.IsNotFound(err) {
+			return models.Withdraws{}, core_err.Rethrow("getting withdraws document", err)
 		}
-		return decode(doc) 
+		return decode(doc)
 	}
 
 }
 
-
-// TODO: consider adding more context info to every core_err.Rethrow() 
+// TODO: consider adding more context info to every core_err.Rethrow()
 
 func NewWithdrawUpdater(updDoc db.Setter, encode mappers.WithdrawEncoder) store.WithdrawUpdater {
 	return func(db db.DB, userId string, withdrawn core.Money) error {
