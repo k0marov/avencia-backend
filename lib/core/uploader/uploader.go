e9dc119206e143e01cb5fdab79d7505bf47ba216
Sam Komarov <sam@skomarov.com>
2022-10-12 13:15:31 +0300

    implement a general uploader handler

--- lib/core/uploader/uploader.go
+++ lib/core/uploader/uploader.go
@@ -1,6 +1,38 @@
 package uploader
 
-// func NewUploader() http.HandlerFunc {
-//   return http_helpers.NewAuthenticatedHandler[http_helpers.NoJSONRequest, ](
-//   )
-// }
+import (
+	"errors"
+	"net/http"
+
+	"github.com/AvenciaLab/avencia-backend/lib/core"
+	"github.com/AvenciaLab/avencia-backend/lib/core/helpers/http_helpers"
+	"github.com/AvenciaLab/avencia-backend/lib/core/helpers/service_helpers"
+	"github.com/AvenciaLab/avencia-backend/lib/core/static_store"
+	"github.com/AvenciaLab/avencia-backend/lib/features/auth/domain/entities"
+)
+
+type UploaderFactory = func(field string, filename string) http.HandlerFunc
+
+type UserFile struct {
+	User entities.User
+	File core.File
+}
+
+func NewUploaderFactory(createFile static_store.StaticFileCreator) UploaderFactory {
+	return func(field string, filename string) http.HandlerFunc {
+		return http_helpers.NewAuthenticatedHandler(
+			func(user entities.User, req *http.Request, _ http_helpers.NoJSONRequest) (UserFile, error) {
+				file := http_helpers.ParseFile(req, field)
+				if !file.IsSet() {
+					return UserFile{}, errors.New("file could not be parsed")
+				}
+				return UserFile{User: user, File: file}, nil
+			},
+			func(uf UserFile) (service_helpers.Nothing, error) {
+				_, err := createFile(uf.File, uf.User.Id, filename)
+				return service_helpers.Nothing{}, err
+			},
+			http_helpers.NoResponseConverter,
+		)
+	}
+}
