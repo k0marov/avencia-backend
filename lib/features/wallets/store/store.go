bba09ab6c17431211488af6aaf2f502afa75973f
Sam Komarov <sam@skomarov.com>
2022-11-10 18:21:20 +0300

    fix maintaing a list of user wallets

--- lib/features/wallets/store/store.go
+++ lib/features/wallets/store/store.go
@@ -12,9 +12,9 @@ import (
 func NewWalletCreator(getDoc db.JsonGetter[UserWalletsModel], updDoc db.JsonUpdater[[]string], setDoc db.JsonSetter[entities.WalletVal]) store.WalletCreator {
 	return func(db db.TDB, w entities.WalletVal) (id string, err error) {
 		id = general_helpers.RandomId()
-		currWalletsPath := []string{"user_wallet", w.OwnerId}
+		currWalletsPath := []string{"user_wallets", w.OwnerId}
 		currWallets, err := getDoc(db, currWalletsPath)
-		if err != nil {
+		if err != nil && !core_err.IsNotFound(err) {
 			return "", err
 		}
 		currWallets.Wallets = append(currWallets.Wallets, id)
@@ -37,29 +37,24 @@ func NewWalletGetter(getDoc db.JsonGetter[entities.WalletVal]) store.WalletGette
 	}
 }
 
-
 // TODO: simplify this
 func NewWalletsGetter(getDoc db.JsonGetter[UserWalletsModel], getWallet store.WalletGetter) store.WalletsGetter {
 	return func(db db.TDB, userId string) ([]entities.Wallet, error) {
-    walletIds, err := getDoc(db, []string{"user_wallets", userId})
-    if core_err.IsNotFound(err) {
-    	return []entities.Wallet{}, nil
-    }
-    if err != nil {
-    	return []entities.Wallet{}, core_err.Rethrow("while getting the list of user wallet ids", err)
-    }
-    wallets := []entities.Wallet{}
-    for _, id := range walletIds.Wallets {
-    	wallet, err := getWallet(db, id)
-    	if err != nil {
+		walletIds, err := getDoc(db, []string{"user_wallets", userId})
+		if err != nil && !core_err.IsNotFound(err) {
+			return []entities.Wallet{}, core_err.Rethrow("while getting the list of user wallet ids", err)
+		}
+		wallets := []entities.Wallet{}
+		for _, id := range walletIds.Wallets {
+			wallet, err := getWallet(db, id)
+			if err != nil {
 				return []entities.Wallet{}, core_err.Rethrow("while getting a wallet by id", err)
-    	}
-    	wallets = append(wallets, wallet)
-    }
-    return wallets, nil
+			}
+			wallets = append(wallets, wallet)
+		}
+		return wallets, nil
 	}
 }
-
 
 func NewBalanceUpdater(updDoc db.JsonUpdater[core.MoneyAmount]) store.BalanceUpdater {
 	return func(db db.TDB, walletId string, newBalance core.MoneyAmount) error {
